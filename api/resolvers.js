be5488b42963958cf1194ea57c356cf608bdcace
akashmair <akash_mair@hotmail.co.uk>
2020-09-06 14:28:56 +0100

    feat(setup): base setup

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,6 +1,30 @@
 
 const resolvers = {
+    Query: {
+        artists: (_, __, {  Artist  }) => {
+            return Artist.find()
+        },
+        songs: (_, __, {  Song  }) => {
+            return Song.find()
+        }
+    },
 
+    Mutation: {
+        newSong: (_, { input }, { Song }) => {
+            return Song.create(input)
+        }
+    },
+
+    Artist: {
+        id: (artist, _, __) => {
+            return artist._id
+        }
+    },
+    Song: {
+        id: (song, _, __) => {
+            return song._id
+        }
+    }
 }
 
 
--- api/schema.js
+++ api/schema.js
@@ -1,6 +1,34 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
+    type Artist {
+        id: ID!
+        name: String!
+        genre: String
+        songs: [Song]
+    }
+
+    type Song {
+        id: ID!
+        title: String!
+        genre: String
+        artist: Artist
+    }
+
+    type Query {
+        artists: [Artist]!
+        songs: [Song]!
+    }
+
+    input NewSongInput {
+        title: String!
+        genre: String!
+        artist: ID!
+    }
+
+    type Mutation {
+        newSong(input: NewSongInput!): Song!
+    }
 `
 
 module.exports = typeDefs
